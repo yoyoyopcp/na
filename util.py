117389e8727e701c836842bcc5a186e7c1d3467a
Paul Carlisle <paul.carlisle@purestorage.com>
2019-09-21 15:18:11 -0600

    better exception in exec_cmd

--- util.py
+++ util.py
@@ -3,4 +3,9 @@ import subprocess
 
 
 def exec_cmd(cmd):
-    return subprocess.check_output(shlex.split(cmd))
+    try:
+        return subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as exc:
+        if not exc.output:
+            raise
+        raise RuntimeError('{}\nOutput: {}'.format(cmd, exc.output.strip()))
