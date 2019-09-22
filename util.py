95da74801cd7e9bd1f5b4faee1d487e26d72d6bf
Paul Carlisle <paul.carlisle@purestorage.com>
2019-09-22 10:22:58 -0600

    create stubs/subparsers

--- util.py
+++ util.py
@@ -1,5 +1,9 @@
+import contextlib
+import dbm
 import shlex
 import subprocess
+
+from na_constants import HOSTS_DB
 
 
 def exec_cmd(cmd):
@@ -9,3 +13,12 @@ def exec_cmd(cmd):
         if not exc.output:
             raise
         raise RuntimeError('{}\nOutput: {}'.format(cmd, exc.output.strip()))
+
+
+@contextlib.contextmanager
+def host_db():
+    db = dbm.open(HOSTS_DB, 'c')
+    try:
+        yield db
+    finally:
+        db.close()
